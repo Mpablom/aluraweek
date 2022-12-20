819671e28103c46701b7d0662b6a2aa89e1e4f79
Mpablom <100524470+Mpablom@users.noreply.github.com>
2022-12-20 00:17:07 -0300

    solucion error en firebase

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 const createProduct = async (jsonProduct) => {
     try {
-        const response = await fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products.json`, {
+        const response = await fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/products.json`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -13,18 +13,18 @@ const createProduct = async (jsonProduct) => {
     }
 }
 
-const readProducts = () => fetch('https://e-commerce-alurageek-default-rtdb.firebaseio.com/products.json').then(response => response.json());
+const readProducts = () => fetch('https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/products.json').then(response => response.json());
 
-const readProduct = (id) => fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/${id}.json`).then(response => response.json());
+const readProduct = (id) => fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/products/${id}.json`).then(response => response.json());
 
 const deleteProduct = (id) => {
-    return fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/${id}.json`, {
+    return fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/products/${id}.json`, {
         method: 'DELETE'
     });
 };
 
 const updateProduct = (id, jsonProduct) => {
-    return fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/${id}.json`, {
+    return fetch(`https://e-commerce-alurageek-default-rtdb.firebaseio.com/products/products/${id}.json`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
